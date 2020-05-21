71216e2c1ffde36bf4ab6a5465c005270e0b1135
Tomchenliping <1349061317@qq.com>
2020-05-21 16:10:25 +0800

    第一次提交

---
+++ src/com/atguigu/java/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+/**
+ * @author clp
+ * @date 2020/5/21/021-16:00
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好啊！");
+    }
+}
